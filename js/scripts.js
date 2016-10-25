b99883275675779aebbba3beed417d614f420a91
Ellie Angell and Frankie Gavin <frankie@>
2016-10-25 10:56:57 -0700

    takes input of val1 and uses val2 to count to results

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
-$(document).ready(function(){
-
-debugger;
-
- var equation = function(val1, val2) {
-   for (var loop= 0; loop <=val1; loop+= val2);
-   //console.log(loop);
+var equation = function(val1, val2) {
+  for (var loop = 0; loop <=val1; loop += val2){
+  $(".strand").append("<li>" + loop + "</li>");
+}
 }
 
+$(document).ready(function(){
 
 
-var countTo = parseInt($("input#multiplier").val());
-var multiply = parseInt($("input#multiplicand").val());
-equation = (countTo, multiply);
- $(".strand").text(equation);
 
-event.preventDefault()
+  $("form#countUp").click(function(event) {
+    var countTo = parseInt($("input#countTo").val());
+    var multiply = parseInt($("input#multiply").val());
+    equation(countTo, multiply);
+    event.preventDefault();
+
+
+
+
+    });
 });
